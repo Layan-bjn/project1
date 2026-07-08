daee8367315f665b8bd8bb542e2371bf410f8f08
hlx73 <hadelturki@hotmail.com>
2026-07-08 10:16:52 +0300

    done

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-i=input(int("choose: 1-add/sub 2-mean/avg "))
+import add.py as add
+import sub.py as sub
+import div.py as div
+import avg.py as avg
+i=input(int("choose: 1-add/sub 2-div/avg "))
 if i == 1:
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
     operation = input("Choose operation (add/sub): ")
     if operation == "add":
-        result = sum(num1 + num2)
-        print(f"The sum is: {result}")
+         num1 = int(input("Enter first number: "))
+         num2 = int(input("Enter second number: "))
+         result = sum(num1,num2)
+         print(f"The sum is: {result}")
     elif operation == "sub":
-        result = sub(num1 - num2)
-        print(f"The difference is: {result}")
+         num1 = int(input("Enter first number: "))
+         num2 = int(input("Enter second number: "))
+         result = sub(num1,num2)
+         print(f"The difference is: {result}")
     else:
         print("Invalid operation selected.")
 elif i == 2:
-    numbers = input("Enter numbers separated by spaces: ")
-    num_list = [int(num) for num in numbers.split()]
-    mean = sum(num_list) / len(num_list)
-    print(f"The mean is: {mean}")
+    operation = input("Choose operation (div/avg): ")
+    if operation == "div":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        result = div(num1,num2)
+        print(f"The quotient is: {result}")
+    elif operation == "avg":
+        list_of_numbers = input("Enter numbers separated by commas: ")
+        result = avg(list_of_numbers)
+        print(f"The difference is: {result}")
+    else:
+        print("Invalid operation selected.")
 else:
     print("Invalid choice. Please select 1 or 2.")
